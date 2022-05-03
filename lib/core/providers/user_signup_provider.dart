5a3fcc98a2657537dc84ba944d31b484d336f038
Daniel Melo <daniel.melo42@outlook.com>
2022-05-03 11:06:51 -0600

    user signup provider added

---
+++ lib/core/providers/user_signup_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flipgrid_mobile/core/domain/entities/user_signup_entity.dart';
+import 'package:flutter/material.dart';
+
+class UserSignupProvider with ChangeNotifier {
+  UserSignUpEntity? _user;
+
+  UserSignUpEntity? get user => _user;
+
+  void setUser(UserSignUpEntity user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  void resetUser(UserSignUpEntity user) {
+    _user = null;
+    notifyListeners();
+  }
+}
