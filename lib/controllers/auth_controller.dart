7c6093f08ddb5c79f23a8230f08e1550b182b921
Daniel Melo <daniel.melo42@outlook.com>
2022-05-03 15:16:05 -0600

    user added to the provider

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,8 +1,10 @@
 import 'package:flipgrid_mobile/core/domain/entities/user_signup_entity.dart';
+import 'package:flipgrid_mobile/core/providers/user_signup_provider.dart';
 import 'package:flipgrid_mobile/views/confirmation/confirmation_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:provider/provider.dart';
 
 part 'auth_controller.g.dart';
 
@@ -15,9 +17,15 @@ abstract class AuthControllerBase with Store {
   @observable
   bool isPasswordObscure = true;
 
+  @observable
+  late UserSignUpEntity user;
+
+  String confirmationGreeting = "Hello!";
+
   signup(BuildContext context, UserSignUpEntity signUpEntity) async {
     isLoading = true;
     await Future.delayed(const Duration(seconds: 1));
+    context.read<UserSignupProvider>().setUser(signUpEntity);
     Navigator.push(
       context,
       PageTransition(
