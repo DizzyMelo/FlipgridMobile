e9bb5a690cdb90567e7277645420b7ada8841de8
Daniel Melo <daniel.melo42@outlook.com>
2022-05-03 11:11:40 -0600

    observables added to the view

--- lib/views/signup/components/signup_form_component.dart
+++ lib/views/signup/components/signup_form_component.dart
@@ -1,23 +1,34 @@
+import 'package:flipgrid_mobile/controllers/auth_controller.dart';
+import 'package:flipgrid_mobile/core/utils/form_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 import '../../../core/components/custom_textfield_component.dart';
 
 class SignupFormComponent extends StatelessWidget {
-  SignupFormComponent({Key? key}) : super(key: key);
+  final TextEditingController firstNameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final TextEditingController websiteController;
+  final AuthController controller;
 
-  final firstNameController = TextEditingController();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final websiteController = TextEditingController();
-
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey formKey;
+  const SignupFormComponent(
+      {Key? key,
+      required this.formKey,
+      required this.firstNameController,
+      required this.emailController,
+      required this.passwordController,
+      required this.websiteController,
+      required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Form(
-        key: _formKey,
+        key: formKey,
         child: Column(
           children: [
             CustomTextfieldComponent(
@@ -29,15 +40,27 @@ class SignupFormComponent extends StatelessWidget {
               label: "Email Address",
               controller: emailController,
               inputType: TextInputType.emailAddress,
+              validator: (String? value) =>
+                  FormValidator.validateEmailAddress(value),
             ),
-            CustomTextfieldComponent(
-              label: "Password",
-              controller: passwordController,
+            Observer(
+              builder: (_) => CustomTextfieldComponent(
+                label: "Password",
+                controller: passwordController,
+                validator: (String? value) =>
+                    FormValidator.validatePassword(value),
+                isSecuredField: true,
+                isObscure: controller.isPasswordObscure,
+                toggleSecuredFied: controller.togglePasswordObscure,
+              ),
             ),
             CustomTextfieldComponent(
               label: "Website",
               controller: websiteController,
               inputType: TextInputType.url,
+              inputAction: TextInputAction.done,
+              validator: (String? value) =>
+                  FormValidator.validateWebsite(value),
             ),
           ],
         ),
--- lib/views/signup/signup_view.dart
+++ lib/views/signup/signup_view.dart
@@ -1,7 +1,10 @@
+import 'package:flipgrid_mobile/controllers/auth_controller.dart';
 import 'package:flipgrid_mobile/core/components/button_component.dart';
 import 'package:flipgrid_mobile/core/components/header_component.dart';
 import 'package:flipgrid_mobile/core/components/scaffold_component.dart';
+import 'package:flipgrid_mobile/core/domain/entities/user_signup_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 import 'components/signup_form_component.dart';
 
@@ -13,6 +16,14 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupViewState extends State<SignupView> {
+  final _formKey = GlobalKey<FormState>();
+  final firstNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final websiteController = TextEditingController();
+
+  final controller = AuthController();
+
   @override
   Widget build(BuildContext context) {
     return ScaffoldComponent(
@@ -26,15 +37,30 @@ class _SignupViewState extends State<SignupView> {
                 subtitle:
                     'Use the form below to submit your portfolio. An email and password are required',
               ),
-              SignupFormComponent(),
+              SignupFormComponent(
+                formKey: _formKey,
+                controller: controller,
+                firstNameController: firstNameController,
+                emailController: emailController,
+                passwordController: passwordController,
+                websiteController: websiteController,
+              ),
             ],
           ),
-          ButtonComponent(
-            title: "Submit",
-            onTap: () => Navigator.pushNamedAndRemoveUntil(
-              context,
-              '/confirmation',
-              (route) => false,
+          Observer(
+            builder: (_) => ButtonComponent(
+              isLoading: controller.isLoading,
+              title: "Submit",
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  UserSignUpEntity signUpEntity = UserSignUpEntity();
+                  signUpEntity.setFirstName = firstNameController.text;
+                  signUpEntity.setEmail = emailController.text;
+                  signUpEntity.setPassword = passwordController.text;
+                  signUpEntity.setWebsite = websiteController.text;
+                  controller.signup(context, signUpEntity);
+                }
+              },
             ),
           )
         ],
