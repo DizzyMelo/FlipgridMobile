68bd6321f9fb6e868a63414c092b912fd4578339
Daniel Melo <daniel.melo42@outlook.com>
2022-05-04 09:08:02 -0600

    password pattern validation added

--- lib/core/utils/validators/password_validator.dart
+++ lib/core/utils/validators/password_validator.dart
@@ -7,6 +7,13 @@ class PasswordValidator implements Validator {
       return "The password is required";
     }
 
+    String pattern = r'(^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,}$)';
+    RegExp regExp = RegExp(pattern);
+
+    if (!regExp.hasMatch(value)) {
+      return 'Password must contain:\n8 characters \n1 symbol \n1 lowercase letter\n1 uppercase letter';
+    }
+
     return null;
   }
 }
