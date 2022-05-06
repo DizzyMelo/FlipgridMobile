226f3bec2f794e9cdbeb053852e3730eaecb69c6
Daniel Melo <daniel.melo42@outlook.com>
2022-05-05 20:01:06 -0600

    routes generation class added

---
+++ lib/core/navigation/routes.dart
@@ -0,0 +1,33 @@
+import 'package:flipgrid_mobile/views/confirmation/confirmation_view.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import '../../controllers/auth_controller.dart';
+import '../../controllers/confirmation_controller.dart';
+import '../../views/signup/signup_view.dart';
+
+class Routes {
+  static Route<dynamic> generateRoutes(RouteSettings settings) {
+    ConfirmationController _confirmationController = ConfirmationController();
+    AuthController _authController = AuthController();
+    switch (settings.name) {
+      case '/signup':
+        return MaterialPageRoute(
+            builder: (context) => SignupView(
+                  controller: _authController,
+                ));
+      case '/confirmation':
+        return PageTransition(
+            child: ConfirmationView(
+              controller: _confirmationController,
+            ),
+            type: PageTransitionType.bottomToTop);
+
+      default:
+        return MaterialPageRoute(
+            builder: (context) => SignupView(
+                  controller: _authController,
+                ));
+    }
+  }
+}
