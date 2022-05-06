55c197b5eed9d64a4883fcd61fa01d4e3d980dfa
Daniel Melo <daniel.melo42@outlook.com>
2022-05-06 12:51:24 -0600

    exception handle implemented to the validation url call

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,5 +1,12 @@
+import 'package:flipgrid_mobile/core/domain/exception/custom_exception.dart';
+import 'package:validators/validators.dart';
+
 class Utils {
   static Uri validateUrl(String url) {
+    if (!isURL(url)) {
+      throw CustomException(detail: "The url is not valid", code: 1);
+    }
+
     if (!url.contains("http")) {
       return Uri.parse("https://$url");
     }
